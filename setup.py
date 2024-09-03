1481ee954979662891d35a05711091d844d68532
Fernando <fernando.y.obara@gmail.com>
2024-09-03 16:27:19 -0300

    third commit

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open("requirements.txt") as f:
 
 
 setup(
-    name="sta_package",
+    name="stat_package",
     version="0.0.1",
     author="fyobara",
     author_email="fernando.y.obara@gmail.com",
     description="O pacote disponibiliza funções para cálculo de medidas de tendência central", long_description=page_description,
-    long_description_content_type="text/markdown", url="my_github_repository_project_link",
+    long_description_content_type="text/markdown", url="https://github.com/fyobara/statistic-package",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
